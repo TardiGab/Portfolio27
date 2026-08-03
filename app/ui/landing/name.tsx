dd708da8b1f8194e5c09a72cf6d03a5e11a972b4
TardiGab <tardigab@gmail.com>
2026-08-03 18:57:05 +0200

    added animation to role text

--- app/ui/landing/name.tsx
+++ app/ui/landing/name.tsx
@@ -1,19 +1,51 @@
 "use client";
 
 import styles from "./name.module.scss";
-import { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/SplitText";
 
 export default function Name() {
-  gsap.registerPlugin(useGSAP); // register the hook to avoid React version discrepancies
+  gsap.registerPlugin(useGSAP);
   gsap.registerPlugin(SplitText);
 
   useGSAP(() => {
     // gsap code here...
-    let split = new SplitText(".gabriel", { type: "chars" });
-    let split2 = new SplitText(".manciu", { type: "chars" });
+    let gabriel = SplitText.create(".gabriel", { type: "words, chars" });
+    let manciu = SplitText.create(".manciu", { type: "words, chars" });
+    let role = SplitText.create(".role", { type: "words" });
+
+    gsap
+      .timeline()
+      .from(gabriel.words, {
+        yPercent: 100,
+        opacity: 0,
+        duration: 0.7,
+        stagger: 0.05,
+        ease: "back.inOut(2)",
+      })
+      .from(
+        manciu.words,
+        {
+          yPercent: 100,
+          opacity: 0,
+          duration: 0.7,
+          stagger: 0.05,
+          ease: "back.inOut(2)",
+        },
+        "-=0.5",
+      )
+      .from(
+        role.words,
+        {
+          yPercent: 100,
+          opacity: 0,
+          duration: 0.8,
+          stagger: 0.05,
+          ease: "back.inOut(2)",
+        },
+        "-=0.7",
+      );
   });
   return (
     <>
@@ -39,7 +71,7 @@ export default function Name() {
         ></div>
       </div>
       <span
-        className={`${styles.role} bg-linear-to-r from-blue-300 to-blue-400 bg-clip-text text-transparent lowercase`}
+        className={`${styles.role} role inline-block overflow-hidden bg-linear-to-r from-blue-300 to-blue-400 bg-clip-text text-transparent lowercase`}
       >
         Développeur frontend junior
       </span>
